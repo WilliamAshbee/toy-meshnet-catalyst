a14f76cc1ff49833838dc8a16f7c288c25e0e641
William A <wsashbee@gmail.com>
2020-07-14 18:39:11 -0400

    toy problem with catalyst minimal

---
+++ toy-2d-catalyst.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from catalyst import dl
+
+print('data')
+# data
+num_samples, num_features = int(1e4), int(1e1)
+X, y = torch.rand(num_samples, num_features), torch.rand(num_samples)
+dataset = TensorDataset(X, y)
+loader = DataLoader(dataset, batch_size=32, num_workers=1)
+loaders = {"train": loader, "valid": loader}
+
+print ('model')
+
+# model, criterion, optimizer, scheduler
+model = torch.nn.Linear(num_features, 1)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters())
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [3, 6])
+
+print ('training')
+
+# model training
+runner = dl.SupervisedRunner()
+runner.train(
+    model=model,
+    criterion=criterion,
+    optimizer=optimizer,
+    scheduler=scheduler,
+    loaders=loaders,
+    logdir="./logdir",
+    num_epochs=8,
+    verbose=True,
+    callbacks=[dl.BatchOverfitCallback(train=10, valid=0.5)]
+)
