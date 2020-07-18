7299213fd7fe1cbddefb311dbb266f121f95c2f8
William A <wsashbee@gmail.com>
2020-07-17 20:51:53 -0400

    exit 0

--- DonutDataset.py
+++ DonutDataset.py
@@ -4,15 +4,15 @@ import pylab as plt
 
 def circle_matrix(radius=10, side=256, w=60):
     xx, yy = torch.meshgrid(torch.arange(side),torch.arange(side))
-    xx = xx.cuda()
-    yy = yy.cuda()
+    xx = xx
+    yy = yy
     x = np.random.randint(side)
     y = np.random.randint(side)
     circle = (xx - x) ** 2 + (yy - y) ** 2
     R = radius**2/4
     w = 600/6400*R
-    donut = torch.logical_and(circle < (R + w), circle > (R - w)).cuda()
-    values = {'donut': donut, 'x': x, 'y': y}
+    donut = torch.logical_and(circle < (R + w), circle > (R - w))
+    values = {'donut': donut, 'x': x, 'y': y, 'radius':radius}
     return values
 
 def plot_all(R=5, side=32, w=10):
@@ -45,12 +45,15 @@ class DonutDataset(torch.utils.data.Dataset):
         side=32
         w=10
         map = circle_matrix(radius=2*R, side=side, w=w)
-        return map['donut']
+        out = [map['x'],map['y'],map['radius']]
+        result = map['donut'].repeat(3, 1, 1).float()
+        assert result.shape == (3,32,32)
+        return result, torch.FloatTensor(out)
     
     @staticmethod
     def displayDonuts(dataset):
         for i in range(100):
-            sample = dataset[i]
+            sample, _ = dataset[i]
             plt.subplot(10,10,i+1)
             plot_all(R=np.random.randint(30))
             plt.axis('off')
--- toy-2d-catalyst.py
+++ toy-2d-catalyst.py
@@ -7,6 +7,7 @@ from torch.utils import data
 from torchvision import transforms
 import argparse
 from CirclesLoad import CirclesLoad
+from DonutDataset import DonutDataset
 from customcriterion import CustomCriterion
 import torchvision.transforms.functional as F
 
@@ -31,8 +32,10 @@ topil_tf = transforms.Compose(
 parser.add_argument('--root', type=str, default='/data/mialab/users/washbee/circles-simple/')
 args = parser.parse_args()
 
-dataset_train = CirclesLoad(args.root,  img_tf, 'train',None)
-dataset_val = CirclesLoad(args.root,  img_tf, 'val',None)
+#dataset_train = CirclesLoad(args.root,  img_tf, 'train',None)
+#dataset_val = CirclesLoad(args.root,  img_tf, 'val',None)
+dataset_train = DonutDataset(2048)
+dataset_val = DonutDataset(2048)
 
 loader_train = data.DataLoader(
     dataset_train, batch_size=mini_batch,
@@ -66,64 +69,8 @@ runner.train(
     scheduler=scheduler,
     loaders=loaders,
     logdir=logdir,
-    num_epochs=5,
+    num_epochs=1,
     verbose=True,
     callbacks=[dl.BatchOverfitCallback(train=10, valid=10)]
 )
-print("finished running")
-print('end of map')
-from PIL import Image
-import numpy as np
-
-from matplotlib.patches import Circle
-import matplotlib.pyplot as plt
-from matplotlib.transforms import Bbox
-import numpy as np
-count = 0
-for el, labels  in iter(loaders["train"]):
-    #el = el*(el<torch.mean(el))
-    print(torch.sum(el))
-    #print('el0', el[0].shape)
-    #print('el1', el[1].shape)
-    #print('len el', len(el))
-    #print(type(el))
-    a = el
-    
-    predictions = model(a.cuda())
-    for i in range(predictions.shape[0]):
-        fig = plt.figure()
-        ax = plt.gca()  # ax = subplot( 1,1,1 )
-        count+=1
-        ########
-        xt = labels[i,0]
-        yt = labels[i,1]
-        rt = labels[i,2]
-        e2 = Circle( xy=(xt, yt), radius= rt )
-        ax.add_artist(e2)
-        #print(ax.bbox)
-        bb = ax.bbox
-        bb._bbox = Bbox(np.array([[0.0, 0.0], [1.0, 1.0]], float))
-        e2.set_clip_box(ax.bbox)
-        e2.set_edgecolor( "black" )
-        e2.set_facecolor( "none" )  # "none" not None
-        e2.set_alpha( 1 )
-        #plt.axis('off')
-        base = '/home/users/washbee1/'
-        
-        ########
-        x = predictions[i,0]
-        y = predictions[i,1]
-        r = predictions[i,2]
-        name = "circleixyr_{}_{:.3f}_{:.3f}_{:.3f}_gt{:.3f}_{:.3f}_{:.3f}p".format(count,xt,yt,rt,x,y,r)
-        e = Circle( xy=(x, y), radius= r )
-        ax.add_artist(e)
-        #print(ax.bbox)
-        e.set_edgecolor( "red" )
-        e.set_facecolor( "none" )  # "none" not None
-        e.set_alpha( 1 )
-        #plt.axis('off')
-        base = '/home/users/washbee1/'
-        plt.savefig("{}{}.jpg".format(base,name), bbox_inches='tight')
-        plt.close(fig)
-    
 
