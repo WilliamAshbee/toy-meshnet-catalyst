69682e189defdd167f278361f65d39342b60e229
William A <wsashbee@gmail.com>
2020-07-19 14:00:14 -0400

    blur added

--- plis-example.py
+++ plis-example.py
@@ -1,30 +1,39 @@
 import numpy as np
 import pylab as plt
+from skimage import filters
 
-def circle_matrix(radius=10, side=256, w=60):
+
+def circle_matrix(radius=10, side=256, w=5, sigma=None):
     xx, yy = np.mgrid[:side, :side]
-    #print(xx)
-    #print(type(xx))
     x = np.random.randint(side)
     y = np.random.randint(side)
     circle = (xx - x) ** 2 + (yy - y) ** 2
-    R = radius**2/4
-    w = 600/6400*R
-    donut = np.logical_and(circle < (R + w), circle > (R - w))
-    values = {'donut': donut, 'x': x, 'y': y}
-    print(values)
-    return values
+    R2 = (radius-w)**2
+    R1 = (radius+w)**2
+    donut = np.logical_and(circle < R1, circle > R2)
+    if sigma is not None:
+        donut = filters.gaussian(donut, sigma=(sigma, sigma))
+    return {'donut': donut, 'x': x, 'y': y}
 
 
-def plot_all(R=5, side=32, w=10):
-    result = circle_matrix(radius=2*R, side=side, w=w)
+def plot_all(R=5, side=64, w=10):
+    sigmas = [None, 1, 2, 3, 4, 5]
+    result = circle_matrix(radius=R, side=side, w=w,
+                           sigma=sigmas[np.random.randint(len(sigmas))])
     plt.imshow(result['donut'], cmap=plt.cm.gray_r)
-    a_circle = plt.Circle((result['y'], result['x']), R, edgecolor='r', facecolor=None, fill=False)
+    a_circle = plt.Circle((result['y'], result['x']), R,
+                          edgecolor='r', facecolor=None, fill=False)
     plt.gca().add_artist(a_circle)
+
+
+side=64
 
 
 for i in range(100):
     plt.subplot(10,10,i+1)
-    plot_all(R=np.random.randint(30))
+    plot_all(R=np.random.randint(side), side=side, w=2)
     plt.axis('off')
+
+
 plt.savefig('finalplot.jpg')
+
