3fdb60de502b81027edf643820cd356e7df2c87b
William A <wsashbee@gmail.com>
2020-08-10 21:45:21 -0400

    splines

--- spline-load.py
+++ spline-load.py
@@ -1,53 +1,67 @@
-import numpy as np
-import scipy.interpolate as si
-import matplotlib.pyplot as plt
+import matplotlib.pylab as P
+import torch
 
+def h_poly_helper(tt):
+  A = torch.tensor([
+      [1, 0, -3, 2],
+      [0, 1, -2, 1],
+      [0, 0, 3, -2],
+      [0, 0, -1, 1]
+      ], dtype=tt[-1].dtype)
+  return [
+    sum( A[i, j]*tt[j] for j in range(4) )
+    for i in range(4) ]
 
-def bspline(cv, n=100, degree=3):
-    """ Calculate n samples on a bspline
+def h_poly(t):
+  tt = [ None for _ in range(4) ]
+  tt[0] = 1
+  for i in range(1, 4):
+    tt[i] = tt[i-1]*t
+  return h_poly_helper(tt)
 
-        cv :      Array ov control vertices
-        n  :      Number of samples to return
-        degree:   Curve degree
-    """
-    print ("cv",cv)
-    cv = np.asarray(cv)
-    count = cv.shape[0]
+def H_poly(t):
+  tt = [ None for _ in range(4) ]
+  tt[0] = t
+  for i in range(1, 4):
+    tt[i] = tt[i-1]*t*i/(i+1)
+  return h_poly_helper(tt)
 
-    # Prevent degree from exceeding count-1, otherwise splev will crash
-    degree = np.clip(degree,1,count-1)
-    print("degree", degree)
-    # Calculate knot vector
-    kv = np.array([0]*degree + [x for x in range(count-degree+1)] + [count-degree]*degree,dtype='int')
-    print("kv",kv)
-    # Calculate query range
-    u = np.linspace(0,(count-degree),n)
-    result = np.array(si.splev(u, (kv,cv.T,degree))).T
-    print(result)
-    # Calculate result
-    return result
+def interp(x, y, xs):
+  m = (y[1:] - y[:-1])/(x[1:] - x[:-1])
+  m = torch.cat([m[[0]], (m[1:] + m[:-1])/2, m[[-1]]])
+  I = P.searchsorted(x[1:], xs)
+  dx = (x[I+1]-x[I])
+  hh = h_poly((xs-x[I])/dx)
+  return hh[0]*y[I] + hh[1]*m[I]*dx + hh[2]*y[I+1] + hh[3]*m[I+1]*dx
 
-colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
+def integ(x, y, xs):
+  m = (y[1:] - y[:-1])/(x[1:] - x[:-1])
+  m = torch.cat([m[[0]], (m[1:] + m[:-1])/2, m[[-1]]])
+  I = P.searchsorted(x[1:], xs)
+  Y = torch.zeros_like(y)
+  Y[1:] = (x[1:]-x[:-1])*(
+      (y[:-1]+y[1:])/2 + (m[:-1] - m[1:])*(x[1:]-x[:-1])/12
+      )
+  Y = Y.cumsum(0)
+  dx = (x[I+1]-x[I])
+  hh = H_poly((xs-x[I])/dx)
+  return Y[I] + dx*(
+      hh[0]*y[I] + hh[1]*m[I]*dx + hh[2]*y[I+1] + hh[3]*m[I+1]*dx
+      )
 
-cv = np.array([[ 50.,  25.],
-   [ 59.,  12.],
-   [ 50.,  10.],
-   [ 57.,   2.],
-   [ 40.,   4.],
-   [ 40.,   14.]])
-
-plt.plot(cv[:,0],cv[:,1], 'o-', label='Control Points')#plots control points
-
-for d in range(1,5):
-    p = bspline(cv,n=100,degree=d)
-    x,y = p.T
-    plt.plot(x,y,'k-',label='Degree %s'%d,color=colors[d%len(colors)])#plots fit points
-
-plt.minorticks_on()
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-#plt.xlim(35, 70)
-#plt.ylim(0, 30)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.savefig('finalplot.png',dpi=300)
+# Example
+if __name__ == "__main__":
+  x = torch.linspace(0, 6, 7)
+  y = x.sin()
+  xs = torch.linspace(0, 6, 101)
+  ys = interp(x, y, xs)
+  Ys = integ(x, y, xs)
+  P.scatter(x, y, label='Samples', color='purple')
+  print(xs,ys)
+  P.plot(xs, ys, label='Interpolated curve')
+  P.plot(xs, xs.sin(), '--', label='True Curve')
+  P.plot(xs, Ys, label='Spline Integral')
+  P.plot(xs, 1-xs.cos(), '--', label='True Integral')
+  P.legend()
+  P.show()
+  P.savefig('finalplot.png',dpi=300)
